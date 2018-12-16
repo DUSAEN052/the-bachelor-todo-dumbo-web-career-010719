35934ea0f5ab4e30a905caf5946a9d37b84f7934
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-16 06:38:09 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |person|
+    if person["status"] == "Winner"
+      return person["name"]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
